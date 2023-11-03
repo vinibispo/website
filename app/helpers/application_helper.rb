03a74ca44dd51da0d1702965bf8b6b0eac04b24e
Vinícius Bispo <vbispo@vmox.com>
2023-11-02 23:27:36 -0300

    fix(paths): add silly methods to add paths

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def projects_path
+    "/projects"
+  end
+
+  def skills_path
+    "/skills"
+  end
+
+  def resume_path
+    "/resume"
+  end
+
+  def contact_path
+    "/contact"
+  end
 end
