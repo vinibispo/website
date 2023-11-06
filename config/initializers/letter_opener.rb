3beaee18a30fbf2b33496903d81645f5f24c719d
Vinícius Bispo <vbispo@vmox.com>
2023-11-06 13:33:16 -0300

    chore(letter_opener): add letter opener to our application for development mode

---
+++ config/initializers/letter_opener.rb
@@ -0,0 +1,6 @@
+if Rails.env.development?
+  Rails.application.configure do
+    config.action_mailer.delivery_method = :letter_opener
+    config.action_mailer.perform_deliveries = true
+  end
+end
