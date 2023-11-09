25971839ad79af61d6a33ec6e7eb7d2ecb8b9a6d
Vinícius Bispo <vbispo@vmox.com>
2023-11-08 21:41:08 -0300

    feat(website): finishing basic website

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,35 @@
 class ProjectsController < ApplicationController
   def index
-    projects = [{id: 1, title: "UpMedal", image: "upmedal.png"}, {id: 2, title: "Embalar", image: "embalar.png"}, {id: 3, title: "SophaWay", image: "sopha.jpg"}, {id: 4, title: "Leveling App", image: "leveling.png"}]
-    render locals: { projects: projects }
+    render locals: { projects: }
     # @projects = Project.all
+  end
+
+  def projects
+    upmedal_description = "
+      A unique e-commerce platform dedicated to athletes and e-athletes was developed using Ruby on Rails, ReactJS, and Material UI. This platform includes features such as easy purchase of rewards, a cashback system, and a strategic upselling mechanism, all contributing to increased profitability and customer loyalty. User experience was prioritized with improved application performance. Rectifying a shipping calculation error ensured accurate cost assessments, safeguarding financial health. The project's success enhanced its foothold in the e-commerce market for athletes, marking an important milestone towards superior profitability.
+      "
+      embalar_description = "
+A robust Warehouse Management System built with cutting-edge technologies like Ruby on Rails, RSpec, ReactJS, and Material UI. This system enhances our main system by streamlining barcode scanning, inventory management, picking and packing processes, and more.
+      "
+
+      sopha_description = "
+      A comprehensive project management application that empowers businesses to streamline collaboration, enhance productivity, and achieve project success. It was written in Ruby on Rails and ReactJS.
+      "
+
+      leveling_app = "
+      A quiz application that helps users to find out their level of knowledge in a specific subject. It was written in NodeJS, Sequelize, and ReactJS. Used by Gamma Academy - Itau.
+      "
+    [
+      { id: 1, title: 'UpMedal', image: 'upmedal.png', description: upmedal_description },
+      { id: 2, title: 'Embalar', image: 'embalar.png', description: embalar_description },
+      { id: 3, title: 'SophaWay', image: 'sopha.jpg', description: sopha_description },
+      { id: 4, title: 'Leveling App', image: 'leveling.png', description: leveling_app }
+    ]
+  end
+
+  def show
+    project = projects.find { |p| p[:id] == params[:id].to_i }
+
+    render locals: { project: }
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
-  def skills_path
-    "/skills"
+  def split_description(description:, classes:)
+    return [] unless description
+    description.split("\n").map do |paragraph|
+      content_tag(:p, paragraph, class: classes)
+    end
+  end
+
+  def title(string = 'ViniBispo')
+    return content_for(:title, string) if string == 'ViniBispo'
+
+    content_for(:title, "#{string} | ViniBispo")
   end
 end
